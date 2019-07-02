de03acebffcbfff66cf767b8038dfc690796c294
cunisia <pierre.cuni@dataiku.com>
2019-07-02 14:34:08 +0200

    Const: creating const class

---
+++ src/utils/const.js
@@ -0,0 +1,13 @@
+class Const {
+    constructor() {
+        this.NEW_GUEST_ID = "NEW_GUESS";
+        this.NEW_GROUP_OPT = "NEW_GROUP_OPT";
+
+        this.GENDER = {
+            MALE: 'M',
+            FEMALE: 'F'
+        };
+    }
+}
+
+export default Object.freeze(new Const());
